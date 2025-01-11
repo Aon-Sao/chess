0fb4845dcc0a3337ed3e179c178a0b021a0e1b1f
Aon-Sao <larsatracy@gmail.com>
2025-01-11 14:50:08 -0700

    enumerated chess directions

--- shared/src/main/java/chess/ChessDirection.java
+++ shared/src/main/java/chess/ChessDirection.java
@@ -1,4 +1,21 @@
 package chess;
 
-public class ChessDirection {
+public enum ChessDirection {
+
+        LEFT      (new int[]{-1,  0}),
+        RIGHT     (new int[]{ 1,  0}),
+        UP        (new int[]{ 0,  1}),
+        DOWN      (new int[]{ 0, -1}),
+        UP_LEFT   (new int[]{-1,  1}),
+        DOWN_LEFT (new int[]{-1, -1}),
+        UP_RIGHT  (new int[]{ 1,  1}),
+        DOWN_RIGHT(new int[]{ 1, -1});
+
+        private int[] vector;
+        public int[] getVector() {
+                return this.vector;
+        }
+        ChessDirection(int[] vector) {
+                this.vector = vector;
+        }
 }
