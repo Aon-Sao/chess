f819e8ac5c78e96a80440b43acf1d1899189f3a8
Aon-Sao <larsatracy@gmail.com>
2025-02-17 16:08:29 -0700

    added bare data model classes

---
+++ shared/src/main/java/model/AuthDataRec.java
@@ -0,0 +1,4 @@
+package model;
+
+public record AuthDataRec(String authToken, String username) {
+}
---
+++ shared/src/main/java/model/UserDataRec.java
@@ -0,0 +1,4 @@
+package model;
+
+public record UserDataRec(String username, String password, String email) {
+}
