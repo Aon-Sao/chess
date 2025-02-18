d07703c86f6ddfe8dafab413f141cdd5bd4c01e3
Aon-Sao <larsatracy@gmail.com>
2025-02-17 17:55:13 -0700

    added 'setters' to GameData record

--- shared/src/main/java/model/GameDataRec.java
+++ shared/src/main/java/model/GameDataRec.java
@@ -3,4 +3,24 @@ package model;
 import chess.ChessGame;
 
 public record GameDataRec(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+
+    public GameDataRec changeGameID(int gameID) {
+        return new GameDataRec(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    public GameDataRec changeWhiteUsername(String whiteUsername) {
+        return new GameDataRec(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    public GameDataRec changeBlackUsername(String blackUsername) {
+        return new GameDataRec(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    public GameDataRec changeGameName(String gameName) {
+        return new GameDataRec(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    public GameDataRec changeGameObj(ChessGame game) {
+        return new GameDataRec(gameID, whiteUsername, blackUsername, gameName, game);
+    }
 }
