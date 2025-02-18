743ebc4c103ad4a03325388ff5b516a60cb7bc9c
Aon-Sao <larsatracy@gmail.com>
2025-02-17 18:02:05 -0700

    added 'setters' to AuthData record

--- shared/src/main/java/model/AuthDataRec.java
+++ shared/src/main/java/model/AuthDataRec.java
@@ -1,4 +1,11 @@
 package model;
 
 public record AuthDataRec(String authToken, String username) {
+    public AuthDataRec changeUsername(String username) {
+        return new AuthDataRec(authToken, username);
+    }
+
+    public AuthDataRec changeToken(String authToken) {
+        return new AuthDataRec(authToken, username);
+    }
 }
