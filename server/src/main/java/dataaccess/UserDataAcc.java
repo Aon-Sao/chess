7d78237a8100c705a0357e12c269c68d2f06d6ae
Aon-Sao <larsatracy@gmail.com>
2025-02-17 17:32:42 -0700

    implemented UserData access object

--- server/src/main/java/dataaccess/UserDataAcc.java
+++ server/src/main/java/dataaccess/UserDataAcc.java
@@ -1,4 +1,68 @@
 package dataaccess;
 
-public class UserDataAcc {
+import model.UserDataRec;
+
+import java.util.*;
+
+public class UserDataAcc implements UserDAO {
+    // Singleton
+    private UserDataAcc instance = null;
+    private UserDataAcc() {
+        clearAll();
+    }
+    public UserDataAcc getInstance() {
+        if (instance == null) {
+            instance = new UserDataAcc();
+        }
+        return instance;
+    }
+
+    private Map<UUID, UserDataRec> users;
+
+    @Override
+    public void createUser(UserDataRec userData) {
+        var id = UUID.randomUUID();
+        users.put(id, userData);
+    }
+
+    @Override
+    public UserDataRec getUser(UUID id) {
+        return users.get(id);
+    }
+
+    @Override
+    public Collection<UserDataRec> listUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public void changeName(UUID id, String username) {
+        var user = getUser(id);
+        user = user.changeUsername(username);
+        users.put(id, user);
+    }
+
+    @Override
+    public void changePass(UUID id, String pass) {
+        var user = getUser(id);
+        user = user.changePassword(pass);
+        users.put(id, user);
+    }
+
+    @Override
+    public void changeEmail(UUID id, String email) {
+        var user = getUser(id);
+        user = user.changeEmail(email);
+        users.put(id, user);
+    }
+
+    @Override
+    public void clearAll() {
+        users = new HashMap<>();
+    }
+
+    @Override
+    public void clearUser(UUID id) {
+        users.remove(id);
+    }
 }
