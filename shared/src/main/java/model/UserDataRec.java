8b34d5b89ddae574a7265d7a48bf91d92aad7440
Aon-Sao <larsatracy@gmail.com>
2025-02-17 17:32:20 -0700

    wrote 'setters' for UserData records

--- shared/src/main/java/model/UserDataRec.java
+++ shared/src/main/java/model/UserDataRec.java
@@ -1,4 +1,14 @@
 package model;
 
 public record UserDataRec(String username, String password, String email) {
+    public UserDataRec changeUsername(String username) {
+        return new UserDataRec(username, password, email);
+    }
+    public UserDataRec changePassword(String password) {
+        return new UserDataRec(username, password, email);
+    }
+    public UserDataRec changeEmail(String email) {
+        return new UserDataRec(username, password, email);
+    }
+
 }
