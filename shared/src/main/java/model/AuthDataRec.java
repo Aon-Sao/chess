1be5f22e41da3b4f24b4ac25826f4b88725cd5cc
Aon-Sao <larsatracy@gmail.com>
2025-03-08 22:28:26 -0700

    removed unused methods

--- shared/src/main/java/model/AuthDataRec.java
+++ shared/src/main/java/model/AuthDataRec.java
@@ -1,11 +1,3 @@
 package model;
 
-public record AuthDataRec(String authToken, String username) {
-    public AuthDataRec changeUsername(String username) {
-        return new AuthDataRec(authToken, username);
-    }
-
-    public AuthDataRec changeToken(String authToken) {
-        return new AuthDataRec(authToken, username);
-    }
-}
+public record AuthDataRec(String authToken, String username) { }
