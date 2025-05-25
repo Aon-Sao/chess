99eac012b20b94332d31cc0fdf18a121d0b4e75e
Aon-Sao <larsatracy@gmail.com>
2025-05-24 20:34:45 -0500

    adapted ServiceHelpers to work with AuthDAOMem

--- server/src/main/java/service/ServiceHelpers.java
+++ server/src/main/java/service/ServiceHelpers.java
@@ -1,6 +1,6 @@
 package service;
 
-import dataaccess.AuthDataAcc;
+import dataaccess.AuthDAOMem;
 import dataaccess.DataAccessException;
 import model.AuthDataRec;
 
@@ -11,7 +11,7 @@ public class ServiceHelpers {
     public static ServiceMessage clearAll() {
         UserService.clear();
         GameService.clear();
-        AuthDataAcc.getInstance().clearAll();
+        new AuthDAOMem().clearAll();
         return ServiceMessage.builder()
                 .setStatusCode(200)
                 .build();
@@ -23,7 +23,7 @@ public class ServiceHelpers {
     }
 
     public static String getUsernameByAuthToken(String authToken) {
-        var authData = AuthDataAcc.getInstance();
+        var authData = new AuthDAOMem();
         for (var auth : authData.listAuths()) {
             if (auth.authToken().equals(authToken)) {
                 return auth.username();
@@ -33,14 +33,14 @@ public class ServiceHelpers {
     }
 
     protected static String authorize(String username) {
-        var authData = AuthDataAcc.getInstance();
+        var authData = new AuthDAOMem();
         var authToken = UUID.randomUUID().toString();
-        authData.createAuth(new AuthDataRec(authToken, username));
+        authData.addAuth(new AuthDataRec(authToken, username));
         return authToken;
     }
 
     public static boolean isAuthorized(ServiceMessage msg) {
-        for (var auth : AuthDataAcc.getInstance().listAuths()) {
+        for (var auth : new AuthDAOMem().listAuths()) {
             // We do not check if username matches, because it is not always provided
             // authTokens should be unique anyway, which is sufficient
             if (auth.authToken().equals(msg.authToken())) {
