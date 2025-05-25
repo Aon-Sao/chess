e6ea5e19198b3a4ba7fd54924576c55adc04c8b2
Aon-Sao <larsatracy@gmail.com>
2025-05-24 21:28:21 -0500

    copied over service tests and did basic regex adaptation for the new DAO design

---
+++ server/src/test/java/service/GameServiceTests.java
@@ -0,0 +1,186 @@
+package service;
+
+import dataaccess.AuthDAOMem;
+import dataaccess.GameDAOMem;
+import dataaccess.UserDAOMem;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameServiceTests {
+
+    @BeforeEach
+    public void initEach() {
+        // Make sure all data stores are clean
+        new AuthDAOMem().clearAll();
+        new GameDAOMem().clearAll();
+        new UserDAOMem().clearAll();
+    }
+
+    @Test
+    public void createGame200() {
+        var loginResult = UserService.register(ServiceMessage.builder()
+                .setUsername("test-username")
+                .setPassword("test-password")
+                .setEmail("test-email")
+                .build()
+        );
+        var authToken = loginResult.authToken();
+        var createResult = GameService.createGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        var gameData = new GameDAOMem();
+        Assertions.assertEquals(200, createResult.statusCode());
+        Assertions.assertEquals(1, gameData.listGames().size());
+    }
+
+    @Test
+    public void createGameBadAuth() {
+        var authToken = "badAuthToken";
+        var createResult = GameService.createGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        var gameData = new GameDAOMem();
+        Assertions.assertEquals(401, createResult.statusCode());
+        Assertions.assertTrue(gameData.listGames().isEmpty());
+    }
+
+    @Test
+    public void listGames200() {
+        var loginResult = UserService.register(ServiceMessage.builder()
+                .setUsername("test-username")
+                .setPassword("test-password")
+                .setEmail("test-email")
+                .build()
+        );
+        var authToken = loginResult.authToken();
+        var gameData = new GameDAOMem();
+        var listResult = GameService.listGames(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .build());
+        Assertions.assertEquals(200, listResult.statusCode());
+        Assertions.assertEquals(0, gameData.listGames().size());
+
+        GameService.createGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        listResult = GameService.listGames(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .build());
+        Assertions.assertEquals(200, listResult.statusCode());
+        Assertions.assertEquals(1, gameData.listGames().size());
+
+        GameService.createGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        listResult = GameService.listGames(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .build());
+        Assertions.assertEquals(200, listResult.statusCode());
+        Assertions.assertEquals(2, gameData.listGames().size());
+    }
+
+    @Test
+    public void listGamesBadAuth() {
+        var authToken = "badAuthToken";
+        var listResult = GameService.listGames(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        Assertions.assertEquals(401, listResult.statusCode());
+        Assertions.assertTrue(listResult.games().isEmpty());
+    }
+
+    @Test
+    public void joinGame200() {
+        var loginResult = UserService.register(ServiceMessage.builder()
+                .setUsername("test-username")
+                .setPassword("test-password")
+                .setEmail("test-email")
+                .build()
+        );
+        var authToken = loginResult.authToken();
+        var createResult = GameService.createGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        int gameID = createResult.gameID();
+        var joinResult = GameService.joinGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameID(gameID)
+                .setPlayerColor("WHITE")
+                .build());
+        Assertions.assertEquals(200, joinResult.statusCode());
+
+        var listResult = GameService.listGames(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .build());
+        Assertions.assertTrue(listResult.games().stream().toList().getFirst().whiteUsername().contains("test-username"));
+        Assertions.assertNull(listResult.games().stream().toList().getFirst().blackUsername());
+    }
+
+    @Test
+    public void joinGame400() {
+        var loginResult = UserService.register(ServiceMessage.builder()
+                .setUsername("test-username")
+                .setPassword("test-password")
+                .setEmail("test-email")
+                .build()
+        );
+        var authToken = loginResult.authToken();
+        var createResult = GameService.createGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        int gameID = createResult.gameID();
+        var joinResult = GameService.joinGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameID(gameID)
+                .setPlayerColor("BLUE")
+                .build());
+        Assertions.assertEquals(400, joinResult.statusCode());
+
+        var listResult = GameService.listGames(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .build());
+        Assertions.assertNull(listResult.games().stream().toList().getFirst().whiteUsername());
+        Assertions.assertNull(listResult.games().stream().toList().getFirst().blackUsername());
+    }
+
+    @Test
+    public void joinGame403() {
+        var loginResult = UserService.register(ServiceMessage.builder()
+                .setUsername("test-username")
+                .setPassword("test-password")
+                .setEmail("test-email")
+                .build()
+        );
+        var authToken = loginResult.authToken();
+        var createResult = GameService.createGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameName("gameName")
+                .build());
+        int gameID = createResult.gameID();
+        GameService.joinGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameID(gameID)
+                .setPlayerColor("WHITE")
+                .build());
+        var joinResult = GameService.joinGame(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .setGameID(gameID)
+                .setPlayerColor("WHITE")
+                .build());
+        Assertions.assertEquals(403, joinResult.statusCode());
+
+        var listResult = GameService.listGames(ServiceMessage.builder()
+                .setAuthToken(authToken)
+                .build());
+        Assertions.assertTrue(listResult.games().stream().toList().getFirst().whiteUsername().contains("test-username"));
+        Assertions.assertNull(listResult.games().stream().toList().getFirst().blackUsername());
+    }
+}
---
+++ server/src/test/java/service/ServiceHelpersTests.java
@@ -0,0 +1,46 @@
+package service;
+
+import dataaccess.AuthDAOMem;
+import dataaccess.GameDAOMem;
+import dataaccess.UserDAOMem;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ServiceHelpersTests {
+
+    @BeforeEach
+    public void initEach() {
+        // Make sure all data stores are clean
+        new AuthDAOMem().clearAll();
+        new GameDAOMem().clearAll();
+        new UserDAOMem().clearAll();
+    }
+
+    @Test
+    public void testClearAll() {
+        var userData = new UserDAOMem();
+        var authData = new AuthDAOMem();
+        var gameData = new GameDAOMem();
+
+        // Create a user
+        UserService.register(ServiceMessage.builder()
+                .setUsername("test-username")
+                .setPassword("test-password")
+                .setEmail("test-email")
+                .build());
+
+        // Create a game
+        GameService.createGame(ServiceMessage.builder()
+                .setGameName("game-name")
+                .build());
+
+        // Clear
+        ServiceHelpers.clearAll();
+
+        // Should be empty
+        Assertions.assertTrue(userData.listUsers().isEmpty());
+        Assertions.assertTrue(authData.listAuths().isEmpty());
+        Assertions.assertTrue(gameData.listGames().isEmpty());
+    }
+}
