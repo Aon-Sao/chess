340750f97b74bd674897facc95afa3ec593c581d
Aon-Sao <larsatracy@gmail.com>
2025-06-09 12:59:22 -0500

    ported over DatabaseManager without connection pooling

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -1,6 +1,8 @@
 package dataaccess;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class DatabaseManager {
@@ -28,6 +30,56 @@ public class DatabaseManager {
             throw new DataAccessException("failed to create database", ex);
         }
     }
+
+    static public void dropDatabase() throws DataAccessException {
+        var statement = "DROP DATABASE IF EXISTS " + databaseName;
+        try (var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to drop database", ex);
+        }
+    }
+
+    private static void createTables() throws DataAccessException {
+        var createStatements = new ArrayList<String>(List.of(
+                "CREATE TABLE IF NOT EXISTS AuthData ( " +
+                        "`id` int NOT NULL AUTO_INCREMENT, " +
+                        "`authToken` varchar(256) NOT NULL, " +
+                        "`username` varchar(256) NOT NULL, " +
+                        "PRIMARY KEY (`id`) )",
+                "CREATE TABLE IF NOT EXISTS UserData ( " +
+                        "`id` int NOT NULL AUTO_INCREMENT, " +
+                        "`username` varchar(256) NOT NULL, " +
+                        "`password` varchar(256) NOT NULL, " +
+                        "`email` varchar(256) NOT NULL, " +
+                        "PRIMARY KEY (`id`) )",
+                "CREATE TABLE IF NOT EXISTS GameData ( " +
+                        "`id` int NOT NULL AUTO_INCREMENT, " +
+                        "`gameID` int NOT NULL, " +
+                        "`whiteUsername` varchar(256) NOT NULL, " +
+                        "`blackUsername` varchar(256) NOT NULL, " +
+                        "`gameName` varchar(256) NOT NULL, " +
+                        "`game` text NOT NULL, " +
+                        "PRIMARY KEY (`id`) )"
+        ));
+
+        try(var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.execute();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public static void init() throws DataAccessException {
+        createDatabase();
+        createTables();
+    }
+
 
     /**
      * Create a connection to the database and sets the catalog based upon the
