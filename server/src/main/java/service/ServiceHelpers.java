885358a189703a7f7591bdcf01d1088638cc67b3
Aon-Sao <larsatracy@gmail.com>
2025-10-08 00:46:38 -0600

    implemented 500 and 401 responses

--- server/src/main/java/service/ServiceHelpers.java
+++ server/src/main/java/service/ServiceHelpers.java
@@ -2,41 +2,40 @@ package service;
 
 import com.google.gson.Gson;
 import io.javalin.http.Context;
-import io.javalin.http.Handler;
 
 import java.util.Map;
 
 public class ServiceHelpers {
 
-    public static void clearAll() {
-        throw new RuntimeException("NYI");
-    }
-
-    // Optional context argument to match arity
-    public static void clearAll(Context context) {
-        clearAll();
-    }
-
     public static boolean isAuthorized(Context context) {
         throw new RuntimeException("NYI");
     }
 
-    public static Handler exceptionWrapper(LambdasCanThrow<Context> func) {
+    public static String authorize(String username) {
+        // Remember to clear out a user's prior token
+        throw new RuntimeException("NYI");
+    }
+
+    public static LambdasCanThrow<Context> exceptionWrapper(LambdasCanThrow<Context> func) {
         return (context) -> {
             try {
                 func.apply(context);
             } catch (Exception e) {
-                throw new RuntimeException(e.getMessage());
+                var msg = Map.of("message", e.getMessage());
+                context.json(new Gson().toJson(msg));
+                context.status(500);
             }
         };
     }
 
-    public static Handler authWrapper(LambdasCanThrow<Context> func) {
+    public static LambdasCanThrow<Context> authWrapper(LambdasCanThrow<Context> func) {
         return (context) -> {
             if (isAuthorized(context)) {
                 func.apply(context);
             } else {
-                throw new RuntimeException("NYI");
+                var msg = Map.of("message", "Error: unauthorized");
+                context.json(new Gson().toJson(msg));
+                context.status(401);
             }
         };
     }
