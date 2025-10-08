8e49bb43b37ffe0d80d1fb3d178173aa90e880a9
Aon-Sao <larsatracy@gmail.com>
2025-10-07 23:43:36 -0600

    set up endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,24 @@
 package server;
 
-import io.javalin.*;
+import io.javalin.Javalin;
+import service.ServiceHelpers;
+
+import static service.ServiceHelpers.authWrapper;
+import static service.ServiceHelpers.exceptionWrapper;
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-        // Register your endpoints and exception handlers here.
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .delete("/db",      exceptionWrapper(ServiceHelpers::clearAll))
+                .post(  "/user",    exceptionWrapper(UserService::register))
+                .post(  "/session", exceptionWrapper(UserService::login))
+                .delete("/session", authWrapper(exceptionWrapper(UserService::logout)))
+                .get(   "/game",    authWrapper(exceptionWrapper(GameService::listGames)))
+                .post(  "/game",    authWrapper(exceptionWrapper(GameService::createGame)))
+                .put(   "/game",    authWrapper(exceptionWrapper(GameService::joinGame)));
 
     }
 
